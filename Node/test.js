254968881f80cf1cebdce893663566d1c6349df2
benjamin <benco00@yahoo.com>
2025-11-19 17:01:29 +0100

    made a change

--- Node/test.js
+++ Node/test.js
@@ -1,3 +1,9 @@
+process.on('uncaughtException', (err) => console.error('💥 Uncaught Exception:', err));
+process.on('unhandledRejection', (reason, promise) => console.error('💥 Unhandled Rejection at:', promise, 'reason:', reason));
+
+console.log('Starting bridge server…');
+
+
 import express from 'express';
 const app = express();
 app.get('/', (req, res) => res.send('Hello'));
